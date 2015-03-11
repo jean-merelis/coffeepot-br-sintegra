83d72709fe3dc54a7cd74b2f8d7ba0b07741f1d6
Jean Merelis <jean.merelis@gmail.com>
2015-03-11 12:16:25 -0300

    ...

--- src/main/java/coffeepot/br/sintegra/writer/WriterFactory.java
+++ src/main/java/coffeepot/br/sintegra/writer/WriterFactory.java
@@ -12,9 +12,9 @@ package coffeepot.br.sintegra.writer;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -41,12 +41,12 @@ public class WriterFactory {
         ObjectWriter beanWriter = FixedLengthWriter.create(w)
                 .withRecordTerminator("\r\n");
 
-        beanWriter.getObjectParserFactory().getHandlerFactory().registerTypeHandlerClassFor(Enum.class, CustomEnumHandler.class);
-        beanWriter.getObjectParserFactory().getHandlerFactory().registerTypeHandlerClassFor(Double.class, CustomDoubleHandler.class);
+        beanWriter.getObjectMapperFactory().getHandlerFactory().registerTypeHandlerClassFor(Enum.class, CustomEnumHandler.class);
+        beanWriter.getObjectMapperFactory().getHandlerFactory().registerTypeHandlerClassFor(Double.class, CustomDoubleHandler.class);
 
-        DefaultDateHandler.setPatternForDateDefault("yyyyMMdd");
-        DefaultDateHandler.setPatternForTimeDefault("HHmmss");
-        DefaultDateHandler.setPatternForDateTimeDefault("yyyyMMddHHmmss");
+        DefaultDateHandler.setPatternDefaultForDate("yyyyMMdd");
+        DefaultDateHandler.setPatternDefaultForTime("HHmmss");
+        DefaultDateHandler.setPatternDefaultForDateTime("yyyyMMddHHmmss");
         DefaultDateHandler.setPatternDefault("yyyyMMdd");
 
         return beanWriter;
